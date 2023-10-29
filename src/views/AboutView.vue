6660555db957d629f6893c8e7833dfef4dd798f2
DesacimangguII <148986847+DesacimangguII@users.noreply.github.com>
2023-10-29 12:01:32 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,7 +11,7 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
     <div class="flex-1 p-10">
       <p class="text-3xl font-semibold my-4">Struktur Desa</p>
-      <img src="@/assets/struktur-desa-contoh.png" alt="" />
+      <img src="@/assets/0-d328-4c1d-b154-256014c34f53.jpg.png" alt="" />
     </div>
   </main>
 </template>
